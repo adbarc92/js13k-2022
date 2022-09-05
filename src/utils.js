b2bba1db083135774ba0b8f8f2614eda21ff434a
adbarc92 <adbarclay92@gmail.com>
2022-09-05 15:52:13 -0600

    Adds timer to utility

--- src/utils.js
+++ src/utils.js
@@ -12,5 +12,40 @@ export class Timer {
     timers.push(this);
   }
 
-  
+  pause() {
+    if (!this.paused) {
+      this.paused = true;
+      this.timestampPause = getNow();
+    }
+  }
+
+  unpause() {
+    if (this.paused) {
+      this.paused = false;
+      this.start -= this.timestampPause;
+      this.timestampPause = 0;
+    }
+  }
+
+  start(duration) {
+    if (this.paused) this.unpause();
+    this.timestampStart = getNow();
+    this.duration = duration ?? this.duration;
+  }
+
+  getPctComplete() {
+    let now = getNow();
+    if (this.paused) now -= this.timestampPause;
+    let diff = now - this.timestampStart;
+    if (diff > this.duration) {
+      diff = this.duration;
+    } else if (diff < 0) {
+      diff = -1;
+    }
+    return Math.min(1, diff / this.duration);
+  }
+
+  isComplete() {
+    return this.getPctComplete() >= 1;
+  }
 }
