d237513b95466c186719e9bed05991bb5d7caf6b
adbarc92 <adbarclay92@gmail.com>
2022-10-19 18:30:11 -0600

    Adds a utility for distance calculation

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 /* Timer */
+
+/**
+ * @param {number} x1 the x coord of the first point
+ * @param {number} y1 the y coord of the first point
+ * @param {number} x2 the x coord of the second point
+ * @param {number} y2 the y coord of the second point
+ *  @returns {number}
+ */
+export const getDistance = (x1, y1, x2, y2) => {
+  return Math.sqrt(Math.pow(x2 - x1) + Math.pow(y2 - y1));
+};
